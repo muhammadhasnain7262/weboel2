dda8af95aa0283ac4091f992f0d56fa6834eb27f
Muhammad Hasnain <64936924+muhammadhasnain7262@users.noreply.github.com>
2022-06-27 14:41:24 +0500

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBRvpczmHZsRXp84tUL-MZCgLRDOrI8AV8",
+    authDomain: "e-clon-b25b6.firebaseapp.com",
+    projectId: "e-clon-b25b6",
+    storageBucket: "e-clon-b25b6.appspot.com",
+    messagingSenderId: "67729206114",
+    appId: "1:67729206114:web:cc65a5d8b2fe87557d5a3b"
+  };
